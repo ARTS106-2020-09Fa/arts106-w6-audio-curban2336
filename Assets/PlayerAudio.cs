0da36eedfa579f29a361e5c9ed35eb679a9111a8
Curban841 <curban841@gmail.com>
2020-10-10 15:46:09 -0800

    Shooting and footsteps

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -18,6 +18,11 @@ public class PlayerAudio : MonoBehaviour
     public LayerMask enemyMask;
 
     bool enemyNear;
+
+    public AudioClip[] grassSteps;
+    public AudioClip[] woodSteps;
+    public AudioClip[] hardSteps;
+
     private void Update()
     {
         RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
@@ -103,6 +108,31 @@ public class PlayerAudio : MonoBehaviour
         if (other.CompareTag("Ambience"))
         {
             ambIdleSnapshot.TransitionTo(0.5f);
+        }
+    }
+
+    public void Footsteps()
+    {
+        RaycastHit hit;
+        int r = Random.Range(0, 3);
+        Ray ray = new Ray(transform.position, -transform.up);
+        if(Physics.Raycast(ray, out hit, 1f))
+        {
+            switch (hit.transform.tag)
+            {
+                case "WoodFloor":
+                    audioS.PlayOneShot(woodSteps[r]);
+                    break;
+                case "HardFloor":
+                    audioS.PlayOneShot(hardSteps[r]);
+                    break;
+                case "GrassFloor":
+                    audioS.PlayOneShot(grassSteps[r]);
+                    break;
+                default:
+                    audioS.PlayOneShot(grassSteps[r]);
+                    break;
+            }
         }
     }
 }
---
+++ Assets/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public AudioClip deathSound;
+    public AudioSource audioS;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(deathSound, transform.position);
+        
+        Destroy(gameObject);
+    }
+}
