925cf496cdee3351712aedfa3c60256708cb2e15
Curban841 <curban841@gmail.com>
2020-10-09 17:14:08 -0800

    Event Music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager manager;
+
+    private void Awake()
+    {
+        manager = this;
+    }
+
+    public AudioSource mainMusic;
+    public AudioSource auxMusic;
+    public AudioSource ambSound;
+
+    public AudioSource eventMusic;
+    public IEnumerator PlayEventMusic()
+    {
+        eventMusic.Play();
+        while (eventMusic.isPlaying)
+        {
+            yield return null;
+        }
+    }
+    
+}
