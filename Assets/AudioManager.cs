538f227574dddeefbad723f89842a36ffbd6a911
Curban841 <curban841@gmail.com>
2020-10-09 23:46:42 -0800

    Event music
    Code is Identical to video but now does not work at all like it is supposed to

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -16,13 +17,29 @@ public class AudioManager : MonoBehaviour
     public AudioSource ambSound;
 
     public AudioSource eventMusic;
+
+    public AudioMixerSnapshot eventSnap;
+    public AudioMixerSnapshot idleSnapshot;
+
+    public bool eventRunning;
+    public bool auxIn;
+
+    public AudioMixerSnapshot currentAudioMixerSnapshot;
     public IEnumerator PlayEventMusic()
     {
+        eventRunning = true;
+        eventSnap.TransitionTo(0.25f);
+        currentAudioMixerSnapshot = eventSnap;
+        yield return new WaitForSeconds(0.3f);
         eventMusic.Play();
         while (eventMusic.isPlaying)
         {
+            
             yield return null;
         }
+        eventRunning = false;
+     //   idleSnapshot.TransitionTo(0.5f);
+        yield break;
     }
     
 }
--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -21,26 +21,59 @@ public class PlayerAudio : MonoBehaviour
     private void Update()
     {
         RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
-        
-        if(hits.Length > 0)
+
+
+        if (hits.Length > 0)
         {
-            if (!enemyNear)
-            {
-                auxInSnapshot.TransitionTo(0.5f);
-                enemyNear = true;
-            }
+            enemyNear = true;
         }
         else
         {
+            enemyNear = false;
+        }
+        if (AudioManager.manager.eventRunning)
+        {
             if (enemyNear)
             {
-                idleSnapshot.TransitionTo(0.5f);
-                enemyNear = false;
+                if (!AudioManager.manager.auxIn)
+                {
+                    auxInSnapshot.TransitionTo(0.5f);
+                    AudioManager.manager.currentAudioMixerSnapshot = auxInSnapshot;
+                    AudioManager.manager.auxIn = true;
+
+                }
+                else
+                {
+                    if(AudioManager.manager.currentAudioMixerSnapshot == AudioManager.manager.eventSnap)
+                    {
+                        auxInSnapshot.TransitionTo(0.5f);
+                        AudioManager.manager.currentAudioMixerSnapshot = auxInSnapshot;
+                        AudioManager.manager.auxIn = true;
+                    }
+                }
+            }
+            else
+            {
+                if (AudioManager.manager.auxIn)
+                {
+                    idleSnapshot.TransitionTo(0.5f);
+                    AudioManager.manager.currentAudioMixerSnapshot = idleSnapshot;
+
+                    AudioManager.manager.auxIn = false;
+                }
+                else
+                {
+                    if (AudioManager.manager.currentAudioMixerSnapshot == AudioManager.manager.eventSnap)
+                    {
+                        idleSnapshot.TransitionTo(0.5f);
+                        AudioManager.manager.currentAudioMixerSnapshot = idleSnapshot;
+
+                        AudioManager.manager.auxIn = false;
+                    }
+                }
             }
         }
     }
-
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
