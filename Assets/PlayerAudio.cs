ad6a6d579f2b3c3dab4423b2f431875042aba0e9
Curban841 <curban841@gmail.com>
2020-10-09 16:25:18 -0800

    Completed Dynamic Soundscaping tutorial

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour
 {
@@ -8,11 +10,50 @@ public class PlayerAudio : MonoBehaviour
 
     public AudioSource audioS;
 
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
+    public AudioMixerSnapshot ambIdleSnapshot;
+    public AudioMixerSnapshot ambInSnapshot;
+
+    public LayerMask enemyMask;
+
+    bool enemyNear;
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
+        
+        if(hits.Length > 0)
+        {
+            if (!enemyNear)
+            {
+                auxInSnapshot.TransitionTo(0.5f);
+                enemyNear = true;
+            }
+        }
+        else
+        {
+            if (enemyNear)
+            {
+                idleSnapshot.TransitionTo(0.5f);
+                enemyNear = false;
+            }
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Enemy Zone"))
+        {
+            auxInSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambInSnapshot.TransitionTo(0.5f);
         }
     }
 
@@ -21,6 +62,14 @@ public class PlayerAudio : MonoBehaviour
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Enemy Zone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambIdleSnapshot.TransitionTo(0.5f);
         }
     }
 }
